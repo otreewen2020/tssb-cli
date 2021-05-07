48a633f653d714f5f3f167423613d9dad8479d26
Tibor Kiss <tibor.kiss@gmail.com>
2021-05-07 17:59:35 +0000

    FIX: tssb.sh runtime fix

--- tssb.sh
+++ tssb.sh
@@ -5,7 +5,7 @@ set -eEuo pipefail
 script=$(realpath ${1})
 script_dir=$(dirname ${script})
 
-IMG=registry.kelly.direct/tssb:latest
+IMG=registry.kelly.direct/tssb-cli:latest
 DATA_DIR=/mnt/wd6/tssb-daily-csvs
 
 docker run \
@@ -13,5 +13,4 @@ docker run \
     --rm \
     -v ${DATA_DIR}:/root/.wine/drive_c/tssb-data/:ro \
     -v ${script_dir}:/root/.wine/drive_c/tssb-scripts/ \
-    --entrypoint /bin/bash \
-    ${IMG} # c:\\tssb-scripts\\$(basename ${script})
+    ${IMG} c:\\tssb-scripts\\$(basename ${script})
