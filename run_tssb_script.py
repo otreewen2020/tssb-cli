9509964dbc0bc89b5adbfbf5e49e81d1c00c12de
Tibor Kiss <tibor.kiss@gmail.com>
2021-05-08 14:36:30 +0000

    FIX: create end of run marker

--- run_tssb_script.py
+++ run_tssb_script.py
@@ -1,17 +1,30 @@
 import sys
 import os
 import time
-from os.path import dirname
+from os.path import dirname, basename
 import pywinauto
 from pywinauto.application import Application
 from pywinauto.keyboard import send_keys
 
 TSSB_SCRIPT = sys.argv[1]
+TSSB_CLI_TEMP = "C:\\tssb-cli-temp"
 
 SLEEP_STEP = 0.05
 TIMEOUT = 60
 
 os.chdir(dirname(TSSB_SCRIPT))
+
+# Copy over the script to \temp and add finish marker
+MARKER_TEXT="TSSB_CLI_END_MARKER"
+script_with_marker = f'{TSSB_CLI_TEMP}\\{basename(TSSB_SCRIPT)}'
+os.mkdir(TSSB_CLI_TEMP)
+with open(script_with_marker, 'w') as f_out:
+    with open(TSSB_SCRIPT, 'r') as f_in:
+        for line in f_in:
+            f_out.write(line)
+    f_out.write(f'\n\nREAD {MARKER_TEXT};\n')
+    
+
 app = Application(backend="win32").start('C:\\tssb\\tssb.exe')
 
 print(f'[DEBUG] Started: {app.windows()} / {app.windows()[0].children()}')
@@ -63,23 +76,27 @@ while timer:
 else:
     raise ValueError("Script file to read dialog did not appear")
 
-print(f"[DEBUG] Starting script `{TSSB_SCRIPT}`: {app.windows()} / {app.windows()[0].children()}")
-send_keys(TSSB_SCRIPT + '{ENTER}')
+print(f"[DEBUG] Starting script `{script_with_marker}`: {app.windows()} / {app.windows()[0].children()}")
+send_keys(script_with_marker + '{ENTER}')
 
 # Wait for processing to start
 while len(app.windows(title=u'Script file to read')) > 0:
     time.sleep(SLEEP_STEP)
 
-# In case of failure a new dialog comes up with okay button.
-# Hence finding ButtonWrapper in the children shows failure.
-error_dialog_ok_buttons = [e for e in app.windows()[0].children() if 'ButtonWrapper' in str(type(e))]
-if error_dialog_ok_buttons:
-    raise ValueError(f'Error while processing: {app.windows()} / {app.windows()[0].children()}')
-
 print(f"[DEBUG] Waiting to finish: {app.windows()} / {app.windows()[0].children()}")
 
 # Wait for processing to finish
-while len(app.windows()[0].children()) != 1:
+while True:
+    print(f"[DEBUG] {app.windows()} / {app.windows()[0].children()}")
+    if any(MARKER_TEXT in str(e) for e in app.windows()[0].children()):
+        break
+
+    # In case of failure a new dialog comes up with okay button.
+    # Hence finding ButtonWrapper in the children shows failure.
+    error_dialog_ok_buttons = [e for e in app.windows()[0].children() if 'ButtonWrapper' in str(type(e))]
+    if error_dialog_ok_buttons:
+        raise ValueError(f'Error while processing: {app.windows()} / {app.windows()[0].children()}')
+
     time.sleep(SLEEP_STEP * 2)
 
 print("TSSB Script Done")
